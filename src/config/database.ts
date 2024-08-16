8bddbeb34630b55796d63f9b80d7e1b354cc6cc9
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-08-16 11:50:23 -0300

    feat: Add database configuration using Sequelize
    The code changes add a new file `database.ts` in the `src/config` directory. This file imports the `dotenv` package for environment configuration and sets up a Sequelize instance for connecting to a PostgreSQL database. The database credentials are retrieved from environment variables.
    
    Based on the recent user commits and repository commits, the commit message follows the convention of starting with a type (`feat` for a new feature) and provides a clear and concise description of the changes made.

---
+++ src/config/database.ts
@@ -0,0 +1,12 @@
+import { config } from "dotenv";
+config();
+import { Sequelize } from "sequelize";
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const sequelize = new Sequelize(DB_NAME!, DB_USER!, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: "postgres"
+})
+
+export default sequelize;
